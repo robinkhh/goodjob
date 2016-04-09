dc7589387fbed42929298103f69bea34b2969d44
robinkhh <robinkhh@gmail.com>
2016-04-09 21:01:31 +0800

    del empty line

--- learn/print.py
+++ learn/print.py
@@ -1,3 +1,12 @@
-mylist=['list',1,2,3,4,5]
-print(mylist)
+def acall(n):
+    while n > 0:
+        yield n
+        n-=1  
+"""
+    for i in range(n):
+        yield i+1
+"""        
+    
+for n in acall(5):
+    print (n)
 
